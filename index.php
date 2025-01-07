e29c60ff8de5350d2fef61ac9940471fe3bb5617
Rizal Lazuardi <work.rizallazuardi45@gmail.com>
2025-01-07 19:55:33 +0700

    main

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start Now Button</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <a href="home/index.php" class="start-now-btn">Start Now</a>
+    </div>
+</body>
+</html>
